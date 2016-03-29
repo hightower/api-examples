616b134f28a08396814bdb54775a300f67a795db
Jonah Burke <jonah@jonahb.com>
2016-03-29 13:37:14 -0400

    Use new API namespace

--- java/src/main/java/com/gethightower/CreateAssetAttachment.java
+++ java/src/main/java/com/gethightower/CreateAssetAttachment.java
@@ -10,7 +10,7 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 public class CreateAssetAttachment {
 
 	public void createAssetAttachment(String assetId, String fileDescription, File file) throws UnirestException {
-		String url = String.format("https://%s/api/external/v1/assets/%s/attachments", Configuration.HOST, assetId);
+		String url = String.format("https://%s/v1/assets/%s/attachments", Configuration.HOST, assetId);
 
 		HttpResponse<JsonNode> response = Unirest.post(url)
 			.header("accept", "application/json")
