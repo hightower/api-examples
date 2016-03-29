83e8b0700344eca19ec350edf59a777cf78893bc
Jonah Burke <jonah@jonahb.com>
2016-03-29 16:14:40 -0400

    Update default host

--- java/src/main/java/com/gethightower/Configuration.java
+++ java/src/main/java/com/gethightower/Configuration.java
@@ -1,7 +1,7 @@
 package com.gethightower;
 
 public class Configuration {
-	final static String HOST = "integrations-sandbox.gethightower.com";
+	final static String HOST = "api-integrations-sandbox.gethightower.com";
     final static boolean SSL = true;
 	final static String API_KEY = "";
 	final static String API_SECRET = "";
